56e06f267decf7822024fe4a4ab1e2ff7b78ffd1
Andrew Hackmann <5140848+bossinc@users.noreply.github.com>
2024-06-24 00:20:32 -0500

    Update pkg/tsdb/mssql/standalone/main.go

--- pkg/tsdb/mssql/standalone/main.go
+++ pkg/tsdb/mssql/standalone/main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 
-	//"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/tsdb/mssql"
 )
